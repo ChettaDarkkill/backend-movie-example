0e24c1c8deac2d4112de1e9717e503072a54ff86
chetta chetta chetta <>
2019-05-01 21:09:58 +0700

    add callback function

--- routes/movies/movies.js
+++ routes/movies/movies.js
@@ -4,12 +4,16 @@ const Joi = require('Joi')
 const connection = require('../config/database')
 
 router.get('/', (req, res) => {
-    connection.query('SELECT * from movies', function (err, rows, fields) {
-        if (err) throw err;
-        res.send(rows)
+    getAllMovies((moviesList) => {
+        res.send(moviesList)
     })
 })
 
-
+function getAllMovies(callback) {
+    connection.query('SELECT * from movies', function (err, rows, fields) {
+        if (err) throw err;
+        callback(rows)
+    })
+}
 
 module.exports = router;
