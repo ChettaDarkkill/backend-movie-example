d42d819fb4421f3abc7ee4644ba61aa131106ff2
chetta chetta chetta <>
2019-05-01 22:58:37 +0700

    add api get movies by id

--- routes/movies/movies.js
+++ routes/movies/movies.js
@@ -5,12 +5,37 @@ const connection = require('../../config/database')
 
 router.get('/', (req, res) => {
     getAllMovies((moviesList) => {
-        res.send(moviesList)
+        if(moviesList.length == 0) {
+            res.status(404).send(JSON.stringify({
+                message: 'not found'
+            }))
+        } else {
+            res.send(moviesList)
+        }
     })
 })
 
+router.get('/:id', (req, res) => {
+    var id = req.params.id
+    getMovieByid(id, ( movie ) => {
+        if(movie.length==0) {
+            res.status(404).send(JSON.stringify({
+                message: "not found"
+            }))
+        } else {
+            res.send(movie)
+        }
+    })
+})
+
+function getMovieByid(id, callback) {
+   connection.query(`SELECT * from movies where id  = ${id}`, (err, rows, fields) => {
+       if(err) throw err;
+       callback(rows)
+   })
+}
 function getAllMovies(callback) {
-    connection.query('SELECT * from movies',  (err, rows, fields)  => {
+    connection.query(`SELECT * from movies`,  (err, rows, fields)  => {
         if (err) throw err;
         callback(rows)
     })
