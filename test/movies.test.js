eea666919f510920201cedba5b2a9252a531c5b4
chetta chetta chetta <>
2019-05-02 16:32:40 +0700

    add test case

--- test/movies.test.js
+++ test/movies.test.js
@@ -1,8 +1,23 @@
 const handleMovies = require('../routes/movies/moviesClass')
+const connection = require('../config/database')
 let handlers = new handleMovies()
 
+
 test('the movies must be empty', () => {
-
-  expect(1).toBe(1)
-
+  connection.connect()
+  function callback(data) {
+    expect(data.length).toBe(4)
+  }
+  handlers.getAllMovies(callback)
+  connection.end()
 });
+
+// test('the movies must be one when call api get movies by id', () => {
+//   connection.connect()
+//     function callback(data) {
+//       expect(data.length).toBe(1)
+//     }
+//     handlers.getMovieByid(1, callback)
+//   connection.end()
+
+// })
