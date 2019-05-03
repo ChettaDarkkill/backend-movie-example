bda8fbe0038eb05ead6b4def2234f8085a09f33a
chetta chetta chetta <>
2019-05-03 11:45:18 +0700

    add middleware

--- routes/movies/movies.js
+++ routes/movies/movies.js
@@ -2,9 +2,10 @@ const express = require('express')
 const router = express.Router()
 const Joi = require('Joi')
 const handleMovies = require('./moviesClass')
+const check = require('../../middleware/jwt')
 
 let handlers = new handleMovies()
-router.get('/', (req, res) => {
+router.get('/', check.checkToken, (req, res) => {
     handlers.getAllMovies((moviesList) => {
         if(moviesList.length == 0) {
             res.status(404).json({
@@ -16,7 +17,7 @@ router.get('/', (req, res) => {
     })
 })
 
-router.get('/:id', (req, res) => {
+router.get('/:id', check.checkToken, (req, res) => {
     var id = req.params.id
     handlers.getMovieByid(id, ( movie ) => {
         if(movie.length==0) {
